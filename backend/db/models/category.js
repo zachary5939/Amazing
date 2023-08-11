e37dcf5e27a968fc9a3fb9699258f70c9df7b255
Zachary <stallings.zach@icloud.com>
2023-08-10 18:27:19 -0700

    eeepy

--- backend/db/models/category.js
+++ backend/db/models/category.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Category.hasMany(models.Product, { foreignKey: 'categoryId', as: 'products' });
+      // Category.hasMany(models.Product, { foreignKey: 'categoryId', as: 'products' });
     }
   }
   Category.init(
