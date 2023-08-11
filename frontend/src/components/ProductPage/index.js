02da2844712b1badd1ef77b57687776f36b4f599
Zachary <stallings.zach@icloud.com>
2023-08-11 09:52:59 -0700

    adding things to cart works

--- frontend/src/components/ProductPage/index.js
+++ frontend/src/components/ProductPage/index.js
@@ -1,27 +1,38 @@
 import './ProductPage.css';
 import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react'; // Import useState
 import { fetchProductById } from '../../store/products';
-import { addToCart } from '../../store/cart'; // Import the addToCart action
+import { addToCart } from '../../store/cart';
 
 function ProductPage() {
     const dispatch = useDispatch();
     const product = useSelector((state) => state.products.productById);
     const { productId } = useParams();
+    const [quantity, setQuantity] = useState(1); // Initialize quantity state
 
     useEffect(() => {
         dispatch(fetchProductById(productId));
     }, [dispatch, productId]);
 
+    //error handling
+    const handleAddToCart = async () => {
+        try {
+            await dispatch(addToCart(product.id, quantity));
+            alert('Successfully added to cart');
+        } catch (error) {
+            alert('Failed to add to cart');
+        }
+    };
+
+    const handleQuantityChange = (event) => {
+        const newQuantity = parseInt(event.target.value);
+        setQuantity(newQuantity);
+    };
+
     if (!product) {
         return <div>Loading...</div>;
     }
-
-    const handleAddToCart = () => {
-        const quantity = 1; // You can adjust the quantity as needed
-        dispatch(addToCart(product.id, quantity));
-    };
 
     return (
         <div className="product-page">
@@ -30,10 +41,24 @@ function ProductPage() {
                 <img src={product.imageUrl} alt={product.name} />
                 <p>{product.description}</p>
                 <p>Price: ${product.price}</p>
+                <div>
+                    <label htmlFor="quantity">Quantity:</label>
+                    <select
+                        id="quantity"
+                        value={quantity}
+                        onChange={handleQuantityChange}
+                    >
+                        {[...Array(10).keys()].map((num) => (
+                            <option key={num + 1} value={num + 1}>
+                                {num + 1}
+                            </option>
+                        ))}
+                    </select>
+                </div>
                 <button onClick={handleAddToCart}>Add to Cart</button>
             </div>
         </div>
-    )
+    );
 }
 
 export default ProductPage;
