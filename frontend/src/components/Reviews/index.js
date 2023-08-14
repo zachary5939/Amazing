f5a026f183d4713047916ed7e70066dc39f17b2d
Zachary <stallings.zach@icloud.com>
2023-08-14 13:22:55 -0700

    reviews page up to par

--- frontend/src/components/Reviews/index.js
+++ frontend/src/components/Reviews/index.js
@@ -1,47 +1,54 @@
 import React, { useEffect } from "react";
-import { useSelector, useDispatch } from 'react-redux';
-import { Link, Redirect } from 'react-router-dom';  // added Redirect
-import { fetchUserRatings} from "../../store/ratings";
-import './reviews.css';
+import { useSelector, useDispatch } from "react-redux";
+import { Link, Redirect } from "react-router-dom";
+import { fetchUserRatings } from "../../store/ratings";
+import "./reviews.css";
 
 function Reviews() {
-    const dispatch = useDispatch();
-    const user = useSelector(state => state.session.user);
-    const userId = user ? user.id : null;;
-    const ratings = useSelector(state => state.ratings.items).filter(rating => rating.userId === userId);
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.session.user);
+  const userId = user ? user.id : null;
+  const ratings = useSelector((state) => state.ratings.items).filter(
+    (rating) => rating.userId === userId
+  );
 
-    useEffect(() => {
-        if (userId) {
-            dispatch(fetchUserRatings(userId));
-        }
-    }, [dispatch, userId]);
+  useEffect(() => {
+    if (userId) {
+      dispatch(fetchUserRatings(userId));
+    }
+  }, [dispatch, userId]);
 
-    // if (!user) {
-    //     return <Redirect to="/login" />;
-    // }
+  // if (!user) {
+  //     return <Redirect to="/login" />;
+  // }
 
-    return (
-        <div>
-            <h2 className='category-name'>My Reviews</h2>
-            <div className="reviews-container">
-                {ratings.map((rating) => (
-                    <div key={rating?.id} className="review-item">
-                        <Link to={`/products/${rating?.productId}`}>
-                            <img src={rating?.product?.imageUrl} alt={rating.product.name} />
-                        </Link>
-                        <div className="review-details">
-                            <h3 className="review-product-name">
-                                <Link to={`/products/${rating?.productId}`}>{rating.product.name}</Link>
-                            </h3>
-                            <p className="review-text">{rating?.text}</p>
-                            <p className="review-rating">Rating: {rating?.rating}</p>
-                            <button className="edit-review-btn">Edit Review</button>
-                        </div>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <h2 className="category-name">My Reviews</h2>
+      <div className="reviews-container">
+        {ratings.map((rating) => (
+          <div key={rating?.id} className="review-item">
+          <div className="review-content">
+              <Link to={`/products/${rating?.productId}`}>
+                  <img src={rating?.product?.imageUrl} alt={rating.product.name} />
+              </Link>
+              <div className="review-details">
+                  <h3 className="review-product-name">
+                      <Link to={`/products/${rating?.productId}`}>{rating.product.name}</Link>
+                  </h3>
+                  <p className="review-text">{rating?.text}</p>
+                  <p className="review-rating">Rating: {rating?.rating}</p>
+              </div>
+          </div>
+          <div className="buttons-container">
+              <button className="review-btn edit-review-btn">Edit Review</button>
+              <button className="review-btn delete-review-btn">Delete Review</button>
+          </div>
+      </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Reviews;
