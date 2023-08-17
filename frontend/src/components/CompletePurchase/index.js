03608db0a2ff6a457e6f2740263b4b0235669d39
Zachary <stallings.zach@icloud.com>
2023-08-17 10:06:57 -0700

    styling changes, redirect and delete cart working

--- frontend/src/components/CompletePurchase/index.js
+++ frontend/src/components/CompletePurchase/index.js
@@ -1,13 +1,22 @@
 import './CompletePurchase.css'
 import React from 'react';
 import logo from "../../img/amazinglogoblack.png";
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import { useSelector } from 'react-redux';
 
 function CompletePurchase() {
-    const sessionUser = useSelector(state => state.session.user);
+    const user = useSelector((state) => state.session.user);
+    const history = useHistory()
+
+    const handleReturnHome = () => {
+        history.push("/")
+    }
+
     return (
-        <div>
-            <h2>Thank you for shopping with us!</h2>
+        <div className="complete-container">
+            <h2 className="complete-heading">Thank you for shopping with us {user?.firstName}!</h2>
+            <img src={logo} alt="Logo" className="complete-logo" />
+            <button className="complete-button" onClick={handleReturnHome}>Return Home</button>
         </div>
     );
 }
